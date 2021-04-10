1bb044b25f0849d39b9591c4d429d5dc5ea94f82
Vu Hoang Giang <itworkjp@gmail.com>
2021-04-10 11:57:12 +0700

    fix build

--- rs.sh
+++ rs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-rm -rf 14/odoo.tar.gz
-tar -zcvf ./14/odoo.tar.gz --exclude .git --exclude "*.log" odoo
-
 rsync -avz --exclude '.git' --exclude 'node_modules' -e "ssh -i odoo.pem" 14 ubuntu@odoo:/home/ubuntu
 ssh  -t -i odoo.pem ubuntu@odoo 'cd /home/ubuntu/14 && docker-compose up --build'
