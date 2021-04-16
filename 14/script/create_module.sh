2247c0be507eba6c546f85d82af446ccd86a6659
Vu Hoang Giang <itworkjp@gmail.com>
2021-04-16 17:01:55 +0700

    refactor code

--- 14/script/create_module.sh
+++ 14/script/create_module.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-docker exec 14_odoo_1 sh -c 'cd /opt/odoo/sources/odoo/ && ./odoo-bin scaffold '$1' addons'
+ODOO_PATH='/opt/odoo/sources/odoo/'
+docker exec 14_odoo_1 sh -c 'cd '$ODOO_PATH' && ./odoo-bin scaffold '$1' addons'
 mkdir -p $2/$1
-docker cp 14_odoo_1:/opt/odoo/sources/odoo/addons/$1 $2
-docker exec 14_odoo_1 sh -c 'rm -rf /opt/odoo/sources/odoo/addons/'$1
+docker cp 14_odoo_1:$ODOO_PATH/addons/$1 $2
+docker exec 14_odoo_1 sh -c 'rm -rf '$ODOO_PATH'/addons/'$1
